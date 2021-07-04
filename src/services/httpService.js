baa6b39f2389ed8dd0ee7d5ec0787dc1054f336d
Rithick B <rithick0907@gmail.com>
2021-07-04 17:25:38 +0530

    API endpoint updated

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,3 +1,3 @@
 export { default as http } from "axios";
 export const baseURL =
-  "https://practice-realtime-db-default-rtdb.firebaseio.com/";
+  "https://practice-firebase-bd6f2-default-rtdb.firebaseio.com/";
